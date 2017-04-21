5a64f92d2c84ae4cdaec530ad2f8af4e85517da0
Michal Vala <mvala@redhat.com>
2017-04-21 14:04:32 +0200

    avoid compiler optimizations in Cast

--- src/main/java/cz/sparko/javagrandejmh/v2/section1/Cast.java
+++ src/main/java/cz/sparko/javagrandejmh/v2/section1/Cast.java
@@ -20,124 +20,125 @@
 
 package cz.sparko.javagrandejmh.v2.section1;
 
-// all benchmarks in this class are completely eliminated by compiler optimizations
+import org.openjdk.jmh.annotations.Benchmark;
+
 public class Cast {
     private static final int INITSIZE = 10000;
 
-    // This test is completely eliminated by compiler optimizations
-//    @Benchmark
+    // i1++ was added to eliminate compiler optimizations
+    @Benchmark
     public float intFloat() {
         int i1 = 6;
         float f1 = 0.0F;
 
         for (int i = 0; i < INITSIZE; i++) {
             /* @formatter:off */
-            f1 = (float) i1; i1 = (int) f1;
-            f1 = (float) i1; i1 = (int) f1;
-            f1 = (float) i1; i1 = (int) f1;
-            f1 = (float) i1; i1 = (int) f1;
-            f1 = (float) i1; i1 = (int) f1;
-            f1 = (float) i1; i1 = (int) f1;
-            f1 = (float) i1; i1 = (int) f1;
-            f1 = (float) i1; i1 = (int) f1;
-            f1 = (float) i1; i1 = (int) f1;
-            f1 = (float) i1; i1 = (int) f1;
-            f1 = (float) i1; i1 = (int) f1;
-            f1 = (float) i1; i1 = (int) f1;
-            f1 = (float) i1; i1 = (int) f1;
-            f1 = (float) i1; i1 = (int) f1;
-            f1 = (float) i1; i1 = (int) f1;
-            f1 = (float) i1; i1 = (int) f1;
+            f1 = (float) i1; i1 = (int) f1; i1++;
+            f1 = (float) i1; i1 = (int) f1; i1++;
+            f1 = (float) i1; i1 = (int) f1; i1++;
+            f1 = (float) i1; i1 = (int) f1; i1++;
+            f1 = (float) i1; i1 = (int) f1; i1++;
+            f1 = (float) i1; i1 = (int) f1; i1++;
+            f1 = (float) i1; i1 = (int) f1; i1++;
+            f1 = (float) i1; i1 = (int) f1; i1++;
+            f1 = (float) i1; i1 = (int) f1; i1++;
+            f1 = (float) i1; i1 = (int) f1; i1++;
+            f1 = (float) i1; i1 = (int) f1; i1++;
+            f1 = (float) i1; i1 = (int) f1; i1++;
+            f1 = (float) i1; i1 = (int) f1; i1++;
+            f1 = (float) i1; i1 = (int) f1; i1++;
+            f1 = (float) i1; i1 = (int) f1; i1++;
+            f1 = (float) i1; i1 = (int) f1; i1++;
             /* @formatter:on */
         }
 
         return f1;
     }
 
-    // This test is completely eliminated by compiler optimizations
+    // this is beaten by compiler optimizations
 //    @Benchmark
     public double intDouble() {
         int i1 = 6;
-        double d1 = 0.0F;
+        double d1 = 0.0D;
 
         for (int i = 0; i < INITSIZE; i++) {
             /* @formatter:off */
-            d1 = (double) i1; i1 =(int) d1;
-            d1 = (double) i1; i1 =(int) d1;
-            d1 = (double) i1; i1 =(int) d1;
-            d1 = (double) i1; i1 =(int) d1;
-            d1 = (double) i1; i1 =(int) d1;
-            d1 = (double) i1; i1 =(int) d1;
-            d1 = (double) i1; i1 =(int) d1;
-            d1 = (double) i1; i1 =(int) d1;
-            d1 = (double) i1; i1 =(int) d1;
-            d1 = (double) i1; i1 =(int) d1;
-            d1 = (double) i1; i1 =(int) d1;
-            d1 = (double) i1; i1 =(int) d1;
-            d1 = (double) i1; i1 =(int) d1;
-            d1 = (double) i1; i1 =(int) d1;
-            d1 = (double) i1; i1 =(int) d1;
-            d1 = (double) i1; i1 =(int) d1;
+            d1 = (double) i1; i1 = (int) d1;
+            d1 = (double) i1; i1 = (int) d1;
+            d1 = (double) i1; i1 = (int) d1;
+            d1 = (double) i1; i1 = (int) d1;
+            d1 = (double) i1; i1 = (int) d1;
+            d1 = (double) i1; i1 = (int) d1;
+            d1 = (double) i1; i1 = (int) d1;
+            d1 = (double) i1; i1 = (int) d1;
+            d1 = (double) i1; i1 = (int) d1;
+            d1 = (double) i1; i1 = (int) d1;
+            d1 = (double) i1; i1 = (int) d1;
+            d1 = (double) i1; i1 = (int) d1;
+            d1 = (double) i1; i1 = (int) d1;
+            d1 = (double) i1; i1 = (int) d1;
+            d1 = (double) i1; i1 = (int) d1;
+            d1 = (double) i1; i1 = (int) d1;
             /* @formatter:on */
         }
 
         return d1;
     }
 
-    // This test is completely eliminated by compiler optimizations
-//    @Benchmark
+    // l1++ was added to eliminate compiler optimizations
+    @Benchmark
     public float longFloat() {
         long l1 = 6;
         float f1 = 0.0F;
 
         for (int i = 0; i < INITSIZE; i++) {
             /* @formatter:off */
-            f1 = (float) l1; l1 =(long) f1;
-            f1 = (float) l1; l1 =(long) f1;
-            f1 = (float) l1; l1 =(long) f1;
-            f1 = (float) l1; l1 =(long) f1;
-            f1 = (float) l1; l1 =(long) f1;
-            f1 = (float) l1; l1 =(long) f1;
-            f1 = (float) l1; l1 =(long) f1;
-            f1 = (float) l1; l1 =(long) f1;
-            f1 = (float) l1; l1 =(long) f1;
-            f1 = (float) l1; l1 =(long) f1;
-            f1 = (float) l1; l1 =(long) f1;
-            f1 = (float) l1; l1 =(long) f1;
-            f1 = (float) l1; l1 =(long) f1;
-            f1 = (float) l1; l1 =(long) f1;
-            f1 = (float) l1; l1 =(long) f1;
-            f1 = (float) l1; l1 =(long) f1;
+            f1 = (float) l1; l1 = (long) f1; l1++;
+            f1 = (float) l1; l1 = (long) f1; l1++;
+            f1 = (float) l1; l1 = (long) f1; l1++;
+            f1 = (float) l1; l1 = (long) f1; l1++;
+            f1 = (float) l1; l1 = (long) f1; l1++;
+            f1 = (float) l1; l1 = (long) f1; l1++;
+            f1 = (float) l1; l1 = (long) f1; l1++;
+            f1 = (float) l1; l1 = (long) f1; l1++;
+            f1 = (float) l1; l1 = (long) f1; l1++;
+            f1 = (float) l1; l1 = (long) f1; l1++;
+            f1 = (float) l1; l1 = (long) f1; l1++;
+            f1 = (float) l1; l1 = (long) f1; l1++;
+            f1 = (float) l1; l1 = (long) f1; l1++;
+            f1 = (float) l1; l1 = (long) f1; l1++;
+            f1 = (float) l1; l1 = (long) f1; l1++;
+            f1 = (float) l1; l1 = (long) f1; l1++;
             /* @formatter:on */
         }
 
         return f1;
     }
 
-    // This test is completely eliminated by compiler optimizations
-//    @Benchmark
+    // l1++ was added to eliminate compiler optimizations
+    @Benchmark
     public double longDouble() {
         long l1 = 6;
-        double d1 = 0.0F;
+        double d1 = 0.0D;
 
         for (int i = 0; i < INITSIZE; i++) {
             /* @formatter:off */
-            d1 = (double) l1; l1 =(long) d1;
-            d1 = (double) l1; l1 =(long) d1;
-            d1 = (double) l1; l1 =(long) d1;
-            d1 = (double) l1; l1 =(long) d1;
-            d1 = (double) l1; l1 =(long) d1;
-            d1 = (double) l1; l1 =(long) d1;
-            d1 = (double) l1; l1 =(long) d1;
-            d1 = (double) l1; l1 =(long) d1;
-            d1 = (double) l1; l1 =(long) d1;
-            d1 = (double) l1; l1 =(long) d1;
-            d1 = (double) l1; l1 =(long) d1;
-            d1 = (double) l1; l1 =(long) d1;
-            d1 = (double) l1; l1 =(long) d1;
-            d1 = (double) l1; l1 =(long) d1;
-            d1 = (double) l1; l1 =(long) d1;
-            d1 = (double) l1; l1 =(long) d1;
+            d1 = (double) l1; l1 = (long) d1; l1++;
+            d1 = (double) l1; l1 = (long) d1; l1++;
+            d1 = (double) l1; l1 = (long) d1; l1++;
+            d1 = (double) l1; l1 = (long) d1; l1++;
+            d1 = (double) l1; l1 = (long) d1; l1++;
+            d1 = (double) l1; l1 = (long) d1; l1++;
+            d1 = (double) l1; l1 = (long) d1; l1++;
+            d1 = (double) l1; l1 = (long) d1; l1++;
+            d1 = (double) l1; l1 = (long) d1; l1++;
+            d1 = (double) l1; l1 = (long) d1; l1++;
+            d1 = (double) l1; l1 = (long) d1; l1++;
+            d1 = (double) l1; l1 = (long) d1; l1++;
+            d1 = (double) l1; l1 = (long) d1; l1++;
+            d1 = (double) l1; l1 = (long) d1; l1++;
+            d1 = (double) l1; l1 = (long) d1; l1++;
+            d1 = (double) l1; l1 = (long) d1; l1++;
             /* @formatter:on */
         }
 
