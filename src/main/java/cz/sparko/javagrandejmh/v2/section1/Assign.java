5f48191d4121b33c2aac11a54bc588e475accc1c
Michal Vala <mvala@redhat.com>
2017-04-21 10:13:01 +0200

    license up, copyright headers

--- src/main/java/cz/sparko/javagrandejmh/v2/section1/Assign.java
+++ src/main/java/cz/sparko/javagrandejmh/v2/section1/Assign.java
@@ -1,3 +1,23 @@
+/**************************************************************************
+ *                                                                         *
+ *             Java Grande Forum Benchmark Suite - Version 2.0             *
+ *                                                                         *
+ *                            produced by                                  *
+ *                                                                         *
+ *                  Java Grande Benchmarking Project                       *
+ *                                                                         *
+ *                                at                                       *
+ *                                                                         *
+ *                Edinburgh Parallel Computing Centre                      *
+ *                                                                         *
+ *                email: epcc-javagrande@epcc.ed.ac.uk                     *
+ *                                                                         *
+ *                                                                         *
+ *      This version copyright (c) The University of Edinburgh, 1999.      *
+ *                         All rights reserved.                            *
+ *                                                                         *
+ **************************************************************************/
+
 package cz.sparko.javagrandejmh.v2.section1;
 
 import org.openjdk.jmh.annotations.Benchmark;
