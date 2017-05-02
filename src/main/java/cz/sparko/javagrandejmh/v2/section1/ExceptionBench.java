d3ebea6ba805c04e1033f4d64402f9dd7ef1c456
Michal Vala <mvala@redhat.com>
2017-05-02 12:58:39 +0200

    Exception benchmark

--- src/main/java/cz/sparko/javagrandejmh/v2/section1/ExceptionBench.java
+++ src/main/java/cz/sparko/javagrandejmh/v2/section1/ExceptionBench.java
@@ -3,27 +3,50 @@ package cz.sparko.javagrandejmh.v2.section1;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.infra.Blackhole;
 
 @State(Scope.Benchmark)
 public class ExceptionBench {
 
     private static final int INITSIZE = 10000;
 
-    Exception except = new Exception();
+    private Exception except = new Exception();
 
     @Benchmark
-    public void exceptionThrow() {
+    public void exceptionThrow(Blackhole bh) {
         for (int i = 0; i < INITSIZE; i++) {
-            /* @formatter:off */
-            try { throw except; } catch (Exception ecatch) {}
-            try { throw except; } catch (Exception ecatch) {}
-            try { throw except; } catch (Exception ecatch) {}
-            try { throw except; } catch (Exception ecatch) {}
-            try { throw except; } catch (Exception ecatch) {}
-            try { throw except; } catch (Exception ecatch) {}
-            try { throw except; } catch (Exception ecatch) {}
-            try { throw except; } catch (Exception ecatch) {}
-            /* @formatter:on */
+            try {
+                throw except;
+            } catch (Exception ecatch) {
+                bh.consume(ecatch);
+            }
         }
+    }
+
+    @Benchmark
+    public void exceptionNew(Blackhole bh) {
+        for (int i = 0; i < INITSIZE; i++) {
+            try {
+                throw new Exception();
+            } catch (Exception ecatch) {
+                bh.consume(ecatch);
+            }
+        }
+    }
+
+    @Benchmark
+    public void exceptionMethod(Blackhole bh) {
+        for (int i = 0; i < INITSIZE; i++) {
+            try {
+                exceptthrow();
+            } catch (Exception ecatch) {
+                bh.consume(ecatch);
+            }
+        }
+    }
+
+    private void exceptthrow() throws Exception {
+        Exception except = new Exception();
+        throw except;
     }
 }
